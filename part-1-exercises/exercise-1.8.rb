dce550d4db524f0e82ff6029bde0718abda3db04
Richard <rich10tom@gmail.com>
2022-02-17 21:00:29 -0600

    Exercise-1.8 completed successfully.

---
+++ part-1-exercises/exercise-1.8.rb
@@ -0,0 +1,18 @@
+# Use 5 built in string methods.
+
+s = 'Hi'
+puts s.replace('Hello')
+
+
+s = 'Name'
+puts s.eql?('name')
+
+
+puts s = 'tow'
+s.include?('f')
+
+
+puts "hello".length
+
+
+puts 'Hello'.reverse
