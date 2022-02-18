e186adecaad8200d9659a38785566d836aff2d21
Richard <rich10tom@gmail.com>
2022-02-17 19:29:45 -0600

    Exercise-1.1 completed successfully.

---
+++ part-1-exercises/exercise-1.1.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Use the method p to print out the string p to the console
+def my_name
+  'p'
+end
+
+puts my_name
+
+# Use the method puts to print out the string puts to the console
+puts 'puts'
+
+# Create a variable called my_name and set it to your name. Print it to the console.
+my_name = 'Richard Thompson'
+puts my_name
+
+# Create a variable called beginning_sentence and set it to My name is . Create another variable called full_sentence
+# and set it equal to the combination of both beginning_sentence and my_name. Use the + operator to do that. Print full
+# sentence.
+beginning_sentence = 'My name is '
+full_sentence = beginning_sentence + my_name
+puts full_sentence
