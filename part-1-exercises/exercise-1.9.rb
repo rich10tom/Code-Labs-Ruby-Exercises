380fec44338192b4a347877d40575a9abd30f529
Richard <rich10tom@gmail.com>
2022-02-17 21:04:58 -0600

    Exercise-1.9 completed successfully.

---
+++ part-1-exercises/exercise-1.9.rb
@@ -0,0 +1,23 @@
+def add(a, b)
+  puts result = a + b
+end
+
+add(12,12)
+
+def subtract(a, b)
+  puts result = a - b
+end
+
+subtract(12,12)
+
+def divide(a, b)
+  puts result = a / b
+end
+
+divide(15,3)
+
+def multiply(a, b)
+  puts result = a * b
+end
+
+multiply(2,5)
